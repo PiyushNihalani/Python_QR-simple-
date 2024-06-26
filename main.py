dc3960e7e9d927eddc903446bbd06ced173ff0f1
Piyush_Nihalani <123048298+PiyushNihalani@users.noreply.github.com>
2024-06-27 02:52:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import qrcode as qr
-img =qr.make("https://drive.google.com/file/d/13M3T3WiL_u_IXqLDSTx5nexYekb1kvC3/view?usp=drivesdk")
-img.save("Piyush_Nihalani_resume.png")
+img =qr.make("your link")# Change the link to whichever link you want to create QR
+img.save("Name of the file.png")#Change to the name of the file and keep ".png" at the end
